5ee11f42b87f6a136b4ac8536b7c3efd3da3528b
Futa Hirakoba <korosuke613613@gmail.com>
2020-03-27 04:11:22 +0900

    都民→県民

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -157,7 +157,7 @@ export default Vue.extend({
         },
         {
           icon: 'mdi-account-multiple',
-          title: this.$t('都民の皆様へ'),
+          title: this.$t('県民の皆様へ'),
           link: 'https://www.metro.tokyo.lg.jp/tosei/tosei/news/2019-ncov.html'
         },
         {
