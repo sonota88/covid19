1e26e7032cd776b8b2ad1f5eeb124c32b8f8d35e
Futa Hirakoba <korosuke613613@gmail.com>
2020-03-27 03:06:01 +0900

    いらないカードを非表示

--- pages/index.vue
+++ pages/index.vue
@@ -21,65 +21,10 @@
     />
     <v-row class="DataBlock">
       <confirmed-cases-details-card />
-      <tested-cases-details-card />
-      <confirmed-cases-attributes-card />
       <confirmed-cases-number-card />
-      <inspection-persons-number-card />
-      <tested-number-card />
-      <telephone-advisory-reports-number-card />
-      <consultation-desk-reports-number-card />
-      <metro-card />
-      <agency-card />
-      <shinjuku-visitors-card />
-      <chiyoda-visitors-card />
+      <confirmed-cases-attributes-card />
     </v-row>
     <v-divider />
-    <v-row class="DataBlock">
-      <v-col cols="12" md="6" class="DataCard">
-        <map-card
-          :title="$t('新宿駅周辺の人口推移（参考値）')"
-          :caption="$t('※7:30〜8:30の平均値')"
-          :title-id="'shinjuku-station-title'"
-          :chart-id="'shinjuku-station-chart'"
-          :map-id="'shinjuku-station-map'"
-          :initial-bounds="[
-            [139.695239, 35.685448],
-            [139.706397, 35.694493]
-          ]"
-          :unit="'人'"
-          :url="'https://ds.yahoo.co.jp/datapolicy/'"
-          :source-link-header="
-            $t('※本データは2020年3月31日までの掲載となります')
-          "
-          :detail-page-url="'/largemap'"
-          :detail-page-string="$t('東京23区の人口推移（参考値）')"
-          :link-string="$t('ヤフー・データソリューション')"
-          :date="''"
-        />
-      </v-col>
-      <v-col cols="12" md="6" class="DataCard">
-        <map-card
-          :title="$t('東京駅周辺の人口推移（参考値）')"
-          :caption="$t('※7:30〜8:30の平均値')"
-          :title-id="'tokyo-station-title'"
-          :chart-id="'tokyo-station-chart'"
-          :map-id="'tokyo-station-map'"
-          :initial-bounds="[
-            [139.761908, 35.676437],
-            [139.773774, 35.686703]
-          ]"
-          :unit="'人'"
-          :url="'https://ds.yahoo.co.jp/datapolicy/'"
-          :source-link-header="
-            $t('※本データは2020年3月31日までの掲載となります')
-          "
-          :detail-page-url="'/largemap'"
-          :detail-page-string="$t('東京23区の人口推移（参考値）')"
-          :link-string="$t('ヤフー・データソリューション')"
-          :date="''"
-        />
-      </v-col>
-    </v-row>
   </div>
 </template>
 
@@ -93,18 +38,9 @@ import Data from '@/data/data.json'
 import News from '@/data/news.json'
 import MapCard from '@/components/MapCard.vue'
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
-import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
-import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
-import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
-import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
-import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
-import MetroCard from '@/components/cards/MetroCard.vue'
-import AgencyCard from '@/components/cards/AgencyCard.vue'
 import { convertDatetimeToISO8601Format } from '@/utils/formatDate'
-import ShinjukuVisitorsCard from '@/components/cards/ShinjukuVisitorsCard.vue'
-import ChiyodaVisitorsCard from '@/components/cards/ChiyodaVisitorsCard.vue'
 
 export default Vue.extend({
   components: {
@@ -113,17 +49,8 @@ export default Vue.extend({
     StaticInfo,
     MapCard,
     ConfirmedCasesDetailsCard,
-    TestedCasesDetailsCard,
     ConfirmedCasesNumberCard,
-    ConfirmedCasesAttributesCard,
-    TestedNumberCard,
-    InspectionPersonsNumberCard,
-    TelephoneAdvisoryReportsNumberCard,
-    ConsultationDeskReportsNumberCard,
-    MetroCard,
-    AgencyCard,
-    ShinjukuVisitorsCard,
-    ChiyodaVisitorsCard
+    ConfirmedCasesAttributesCard
   },
   data() {
     const data = {
