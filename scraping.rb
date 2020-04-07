9bb6a50acab3b48b640d3b2017ea97b9323d98ec
koogawa <koogawa.app@gmail.com>
2020-04-08 02:36:47 +0900

    Fix error

--- scraping.rb
+++ scraping.rb
@@ -6,7 +6,7 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :remote, desired_capabilities: :chrome, url: "http://#{ENV['SELENIUM_HOST']}:4444/wd/hub"
 
 # スクレイピング
-driver.navigate.to((ENV['URL'])
+driver.navigate.to(ENV['URL'])
 list_table = driver.find_element(:class => "list_table")
 dates = list_table.find_elements(:class => "date")
 urls = list_table.find_elements(:tag_name => "a")
