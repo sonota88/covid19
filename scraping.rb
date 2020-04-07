71c69a7b77f5dfe3cac20c0f5726bfada3e1147e
koogawa <koogawa.app@gmail.com>
2020-04-08 02:33:02 +0900

    Comment out

--- scraping.rb
+++ scraping.rb
@@ -1,35 +1,19 @@
 require 'selenium-webdriver'
-require 'date'
-require 'dotenv/load'
-require 'google_drive'
+# require 'date'
+# require 'dotenv/load'
+# require 'google_drive'
 
 driver = Selenium::WebDriver.for :remote, desired_capabilities: :chrome, url: "http://#{ENV['SELENIUM_HOST']}:4444/wd/hub"
 
-def driver.find_and_wait_element(how, what)
-  Selenium::WebDriver::Wait.new(timeout: 10).until{ find_element(how, what) }
-  sleep 2 # コンテンツがヌルっと出てくるのでちょっと待たないとクリック等ができない
-  find_element(how, what)
-end
-
-###
-
-# ログイン
-driver.navigate.to('https://www.pref.miyazaki.lg.jp/kenko/hoken/kansensho/covid19/hassei.html') #(ENV['URL'])
-data = driver.page_source
-puts 'こんにちは'
-puts '今日の天気は'
+# スクレイピング
+driver.navigate.to((ENV['URL'])
 list_table = driver.find_element(:class => "list_table")
 dates = list_table.find_elements(:class => "date")
 urls = list_table.find_elements(:tag_name => "a")
 texts = list_table.find_elements(:tag_name => "a")
-puts dates
 count = dates.length - 1
 newsItems = []
 for i in 0..count do
-  p i
-  p dates[i].text
-  p urls[i].attribute("href")
-  p texts[i].text
   newsItem = { "date" => dates[i].text, "url" => urls[i].attribute("href"), "text" => texts[i].text }
   newsItems.push(newsItem)
 end
