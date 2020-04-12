c5d665de738fb84600235407fd4a66266d6da35d
koogawa <koogawa.app@gmail.com>
2020-04-12 21:00:28 +0900

    puts uls[i].text

--- scraping_data.rb
+++ scraping_data.rb
@@ -5,13 +5,20 @@ driver = Selenium::WebDriver.for :remote, desired_capabilities: :chrome, url: "h
 # スクレイピング
 driver.navigate.to(ENV['URL'])
 list_table = driver.find_element(:class => "list_table")
-# dates = list_table.find_elements(:class => "date")
 urls = list_table.find_elements(:tag_name => "a")
 # texts = list_table.find_elements(:tag_name => "a")
-count = urls.length - 1
-url = urls[count].attribute("href")
+# count = urls.length - 1
+url = urls[0].attribute("href")
 puts "news url = "
 puts url
+
+driver.navigate.to(url)
+# noicon
+uls = list_table.find_elements(:class => "noicon")
+count = uls.length - 1
+for i in 0..count do
+  puts uls[i].text
+end
 
 exit
 
