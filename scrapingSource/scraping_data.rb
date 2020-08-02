2cc7b0ce3571d569abffd849e4a8c4d29828ed53
Futa Hirakoba <korosuke613613@gmail.com>
2020-08-02 15:46:37 +0900

    fix: support scraping number of positive for new website

--- scrapingSource/scraping_data.rb
+++ scrapingSource/scraping_data.rb
@@ -6,41 +6,83 @@ driver = Selenium::WebDriver.for :remote, desired_capabilities: :chrome, url: "h
 
 # 最新ニュースのURL取得
 driver.navigate.to(ENV['URL'])
-list_table = driver.find_element(:class => "list_table")
-urls = list_table.find_elements(:tag_name => "a")
-url = urls[0].attribute("href")
+list_table = driver.find_element(:class => "datatable")
+trs = list_table.find_elements(:tag_name => "tr")
 
-# 最新ニュースをスクレイピング
-driver.navigate.to(url)
-uls = driver.find_elements(:class => "noicon")
-count = uls.length - 1
+count = trs.length - 1
 datas = []
-for i in 0..count do
-  data = { "リリース日" => today, "退院" => "入院中", "date" => today }
-  ul = uls[i]
+dates = {}
+statuses = {"入院中"=>0, "宿泊療養中"=>0, "退院" => 0, "死亡" => 0}
 
-  address = ul.text.match(/居住地(.+)/)
-  if address
-    data["居住地"] = address[1]
+for i in 1..count do
+  # 1行目は列名にあたるのでスキップする
+  data = {}
+  tr = trs[i]
+  tds = tr.find_elements(:tag_name => "td")
+
+  no = tds[0].text
+
+  date_splits = tds[1].text.split("月")
+  month = date_splits[0]
+  if month.length == 1 then
+    month = "0#{month}"
+  end
+  day = date_splits[1].split("日")[0]
+  if day.length == 1 then
+    day = "0#{day}"
+  end
+  date = "2020-#{month}-#{day}T08:00:00.000Z"
+
+  if dates.has_key?(date) == false then
+    dates[date] = 1
   else
-    next
+    dates[date] += 1
   end
 
-  age = ul.text.match(/年齢(.+)/)
-  if age
-    data["年代"] = age[1]
+  age = tds[2].text
+  gender = tds[3].text
+  address = tds[4].text.split("\n")[0]
+  status = tds[5].text
+
+  case status
+  when "入院調整中", "入院中",
+    statuses["入院中"] += 1
+  when "宿泊療養中"
+    statuses["宿泊療養中"] += 1
+  when "死亡"
+    statuses["死亡"] += 1
   else
-    next
+    statuses["退院"] += 1
   end
 
-  gender = ul.text.match(/性別(.+)/)
-  if gender
-    data["性別"] = gender[1]
-  else
-    next
-  end
+  contactStatus = tds[6].text
+
+  data["NO"] = no
+  data["リリース日"] = date
+  data["居住地"] = address
+  data["年代"] = age
+  data["性別"] = gender
+  data["退院"] = status
+  data["接触状況"] = contactStatus
 
   datas.push(data)
+end
+
+reverseDates = Hash[dates.to_a.reverse]
+dataHash = []
+reverseDates.each do |key, value|
+  dataHash.push({
+                    "日付"=> key,
+                    "小計"=> value
+                })
+end
+
+statusHash = []
+statuses.each do |key, value|
+  statusHash.push({
+                      "attr"=>key,
+                      "value"=>value
+                  })
 end
 
 data_count = datas.length
@@ -53,11 +95,10 @@ end
 # data.json を更新
 data_hash["lastUpdate"] = today
 data_hash["patients"]["date"] = today
-data_hash["patients"]["data"].concat(datas)
-data_hash["main_summary"]["children"][0]["value"] = data_hash["main_summary"]["children"][0]["value"] + data_count
-data_hash["main_summary"]["children"][0]["children"][0]["value"] = data_hash["main_summary"]["children"][0]["children"][0]["value"] + data_count
-data_hash["main_summary"]["children"][0]["children"][0]["children"][0]["value"] = data_hash["main_summary"]["children"][0]["children"][0]["children"][0]["value"] + data_count
-data_hash["patients_summary"]["data"].push({ "日付" => today, "小計" => data_count })
+data_hash["patients"]["data"] = datas
+data_hash["main_summary"]["children"][0]["value"] = data_count
+data_hash["main_summary"]["children"][0]["children"] = statusHash
+data_hash["patients_summary"]["data"] = dataHash
 
 data_json = JSON.pretty_generate(data_hash, {:indent => "    "})
 File.open("data/data.json", mode = "w") { |f|
