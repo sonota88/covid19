6f9fbd7eb9060ec833ff0571d018dbba182d517e
Futa Hirakoba <korosuke613613@gmail.com>
2020-08-02 16:57:23 +0900

    fix: number of inspectors

--- scrapingSource/scraping.rb
+++ scrapingSource/scraping.rb
@@ -25,5 +25,21 @@ File.open("data/news.json", mode = "w") { |f|
   f.write(news_json)
 }
 
+pcr_table = driver.find_elements(:class => "datatable").last
+rows = pcr_table.find_elements(:tag_name => "tr").last
+total = rows.find_element(:tag_name => "td").find_element(:tag_name => "p")
+
+data_hash = {}
+File.open("data/data.json") do |file|
+  data_hash = JSON.load(file)
+end
+
+data_hash["main_summary"]["value"] = total.text.delete(",").to_i
+
+data_json = JSON.pretty_generate(data_hash, {:indent => "    "})
+File.open("data/data.json", mode = "w") { |f|
+  f.write(data_json)
+}
+
 exit
 
